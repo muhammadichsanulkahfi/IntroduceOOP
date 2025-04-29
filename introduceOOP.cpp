7dcb52e0b793d948a5ba1b13ccde7a3ec8ef2b2c
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-04-29 18:57:00 +0700

    membuat aksesmodifier publick

--- introduceOOP.cpp
+++ introduceOOP.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 class Mahasiwa{
+    public :
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData(){
+        cout << "Nim : " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
 
 };
 int main(){
+    Mahasiwa mhs;
 
-}
+    mhs.nim = 2024;
+    mhs.nama = "abra";
+    mhs.nilai = 80.5;
+
+    mhs.printData();
+    
+};
